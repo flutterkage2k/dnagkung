505e1766497bec94809c011dcfa89cc5b14fa816
flutterkage2k <flutterkage2k@gmail.com>
2021-10-31 21:25:35 +0900

    run App

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# dnagkung
+# 당근마켓클
 
-A new Flutter project.
+코딩파파 제작, 당근마켓 클론 입니다.
 
 ## Getting Started
 
@@ -14,3 +14,4 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+론
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,12 +10,15 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => GetMaterialApp(
-        theme: ThemeData(
-          textTheme: GoogleFonts.notoSansTextTheme(
-            Theme.of(context).textTheme,
-          ),
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: ThemeData(fontFamily: 'DoHyeon'),
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Text("home"),
         ),
-        // home:
-      );
+      ),
+    );
+  }
 }
