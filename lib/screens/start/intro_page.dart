b9931b7ebfbd40a38ce9f15d9541444c71b7177f
flutterkage2k <jinheesung1@gmail.com>
2022-01-14 22:17:27 +0900

    complete 22

--- lib/screens/start/intro_page.dart
+++ lib/screens/start/intro_page.dart
@@ -1,22 +1,52 @@
+import 'package:dnagkung/utils/logger.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({Key? key}) : super(key: key);
 
+  void onButtonClick() {
+    logger.d('on text button  clicked!!');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text('토토토마켓'),
-        ExtendedImage.asset('assets/imgs/carrot_intro.png'),
-        Text('우리 동네 중고 직거래 토토토마켓'),
-        Text('직거래 마켓'),
-        TextButton(
-          onPressed: () {},
-          child: Text('시작'),
-        )
-      ],
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(
+              '토마토마켓',
+              style: TextStyle(
+                  fontSize: 30, fontWeight: FontWeight.bold, color: Colors.red),
+            ),
+            ExtendedImage.asset('assets/imgs/carrot_intro.png'),
+            Text(
+              '우리 동네 중고 직거래 토토토마켓',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              '직거래 마켓',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextButton(
+                  onPressed: onButtonClick,
+                  style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                  child: Text(
+                    '시작',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
