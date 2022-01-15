55685221f979759365834d7e140be5160d932a1a
flutterkage2k <jinheesung1@gmail.com>
2022-01-15 09:50:40 +0900

    27 끝

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -2,20 +2,27 @@ import 'package:dnagkung/screens/start/intro_page.dart';
 import 'package:flutter/material.dart';
 
 class AuthScreen extends StatelessWidget {
-  const AuthScreen({Key? key}) : super(key: key);
+  AuthScreen({Key? key}) : super(key: key);
+
+  final PageController _pageController = PageController();
+  //viewportFraction = 다음페이지를 조금 보이게 만들어준다.
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(children: [
-        IntroPage(),
-        Container(
-          color: Colors.accents[2],
-        ),
-        Container(
-          color: Colors.accents[5],
-        )
-      ]),
+      body: PageView(
+          physics: NeverScrollableScrollPhysics(),
+          //physics: NeverScrollableScrollPhysics(), 손가락으로 스와이프는 막는다.
+          controller: _pageController,
+          children: [
+            IntroPage(_pageController),
+            Container(
+              color: Colors.accents[2],
+            ),
+            Container(
+              color: Colors.accents[5],
+            )
+          ]),
     );
   }
 }
