4dd7d45ee248359a3751447e9815840b67a651bc
flutterkage2k <jinheesung1@gmail.com>
2022-02-04 20:50:50 +0900

    29.complete

--- lib/screens/start/address_page.dart
+++ lib/screens/start/address_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AddressPage extends StatelessWidget {
@@ -16,13 +17,32 @@ class AddressPage extends StatelessWidget {
                 color: Colors.grey,
               ),
               hintText: "도로명으로 검색",
-              hintStyle: TextStyle(color: (Theme.of(context).hintColor)),
+              hintStyle: TextStyle(color: Theme.of(context).hintColor),
               border: UnderlineInputBorder(
                 borderSide: BorderSide(color: Colors.grey),
               ),
               prefixIconConstraints:
                   BoxConstraints(minWidth: 24, minHeight: 24),
             ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextButton.icon(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.compass,
+                  color: Colors.white,
+                  size: 20,
+                ),
+                label: Text(
+                  '현재 위치 찾기',
+                  style: Theme.of(context).textTheme.button,
+                ),
+                style: TextButton.styleFrom(
+                    backgroundColor: Theme.of(context).primaryColor),
+              ),
+            ],
           )
         ],
       ),
