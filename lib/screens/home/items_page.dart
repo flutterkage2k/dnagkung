6b74d722f8ba76ff434693668c9440e77c79e660
flutterkage2k <jinheesung1@gmail.com>
2022-02-12 13:00:25 +0900

    64 android firebase

--- lib/screens/home/items_page.dart
+++ lib/screens/home/items_page.dart
@@ -8,79 +8,93 @@ class ItemsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      padding: EdgeInsets.all(common_padding),
-      separatorBuilder: (context, index) {
-        return Divider(
-          height: common_padding * 2 + 1,
-          thickness: 1,
-          color: Colors.grey[200],
-          indent: common_sm_padding,
-          endIndent: common_sm_padding,
-        );
-      },
-      itemBuilder: (context, index) {
-        return SizedBox(
-          height: 100,
-          child: Row(
-            children: [
-              ExtendedImage.network('https://picsum.photos/100'),
-              SizedBox(
-                width: common_sm_padding,
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'work',
-                      style: Theme.of(context).textTheme.subtitle1,
-                    ),
-                    Text(
-                      '53days before',
-                      style: Theme.of(context).textTheme.subtitle2,
-                    ),
-                    Text('5000 won'),
-                    Expanded(child: Container()),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        Size size = MediaQuery.of(context).size;
+        final imgSize = size.width / 4;
+        return ListView.separated(
+          padding: EdgeInsets.all(common_padding),
+          separatorBuilder: (context, index) {
+            return Divider(
+              height: common_padding * 2 + 1,
+              thickness: 1,
+              color: Colors.grey[200],
+              indent: common_sm_padding,
+              endIndent: common_sm_padding,
+            );
+          },
+          itemBuilder: (context, index) {
+            return SizedBox(
+              //비율에 맞도록 변경해보자.
+              height: imgSize,
+              child: Row(
+                children: [
+                  SizedBox(
+                      height: imgSize,
+                      width: imgSize,
+                      child: ExtendedImage.network(
+                        'https://picsum.photos/100',
+                        shape: BoxShape.rectangle, //rectangle 로 해야 꼭지점 부분만 없어짐
+                        borderRadius: BorderRadius.circular(12),
+                      )),
+                  SizedBox(
+                    width: common_sm_padding,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        SizedBox(
-                          height: 15,
-                          //FittedBox기능은 원하는 사이즈로 줄여준다. 즉, 위에 SizedBox의 height 사이즈에 맞게
-                          child: FittedBox(
-                            child: Row(
-                              children: [
-                                Icon(
-                                  CupertinoIcons.chat_bubble_2,
-                                  color: Colors.grey,
-                                ),
-                                Text(
-                                  '23',
-                                  style: TextStyle(color: Colors.grey),
-                                ),
-                                Icon(
-                                  CupertinoIcons.heart,
-                                  color: Colors.grey,
-                                ),
-                                Text(
-                                  '30',
-                                  style: TextStyle(color: Colors.grey),
-                                )
-                              ],
-                            ),
-                          ),
+                        Text(
+                          'work',
+                          style: Theme.of(context).textTheme.subtitle1,
                         ),
+                        Text(
+                          '53days before',
+                          style: Theme.of(context).textTheme.subtitle2,
+                        ),
+                        Text('5000 won'),
+                        Expanded(child: Container()),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            SizedBox(
+                              height: 15,
+                              //FittedBox기능은 원하는 사이즈로 줄여준다. 즉, 위에 SizedBox의 height 사이즈에 맞게
+                              child: FittedBox(
+                                child: Row(
+                                  children: [
+                                    Icon(
+                                      CupertinoIcons.chat_bubble_2,
+                                      color: Colors.grey,
+                                    ),
+                                    Text(
+                                      '23',
+                                      style: TextStyle(color: Colors.grey),
+                                    ),
+                                    Icon(
+                                      CupertinoIcons.heart,
+                                      color: Colors.grey,
+                                    ),
+                                    Text(
+                                      '30',
+                                      style: TextStyle(color: Colors.grey),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        )
                       ],
-                    )
-                  ],
-                ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            );
+          },
+          itemCount: 10,
         );
       },
-      itemCount: 10,
     );
   }
 }
